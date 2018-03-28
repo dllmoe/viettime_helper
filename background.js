ea89eb8f7dd3605339f1bea6433b25df487ae26b
dllmoe <dllmoe@163.com>
2018-03-28 23:24:54 +0800

    .

--- background.js
+++ background.js
@@ -5,10 +5,16 @@ function getRate(callback) {
         if (req.readyState == 4 && req.status == 200) {
             try {
                 var result = JSON.parse(req.responseText)
+                var found = result.find(function (item) {
+                    return item.from == 'CNY' && item.to == 'VND'
+                })
+                if (found) {
+                    var rate = +found.rate
+                }
             } catch (err) {
                 console.log(err)
             }
-            callback(result || false)
+            callback(rate || 3600)
         }
     }
     req.send()
@@ -19,22 +25,20 @@ chrome.runtime.onMessage.addListener(function (msg, sender, response) {
 
     if (localStorage.rate) {
         var rate = JSON.parse(localStorage.rate)
-        var lastUpdate = new Date(rate.updatetime + ' GMT+0800')
+        var lastUpdate = new Date(rate.date)
         if (Date.now() - lastUpdate.getTime > 86400000) {
             getRate(function (rate) {
-                if (!rate) return response(false)
-                localStorage.rate = JSON.stringify(rate)
-                response(+rate.rate)
+                localStorage.rate = JSON.stringify({ rate: rate, date: Date.now() })
+                response(rate)
             })
             return true
         } else {
-            response(+rate.rate)
+            response(rate.rate)
         }
     } else {
         getRate(function (rate) {
-            if (!rate) return response(false)
-            localStorage.rate = JSON.stringify(rate)
-            response(+rate.rate)
+            localStorage.rate = JSON.stringify({ rate: rate, date: Date.now() })
+            response(rate)
         })
         return true
     }
